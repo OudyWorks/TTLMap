2350cc6ef6c7fc5ea94ff00c95f338dc994fbf3e
Ayoub Oudmane <ayoub@oudmane.me>
2018-09-01 16:40:24 +0100

    #

---
+++ source/index.js
@@ -0,0 +1,41 @@
+const throttle = require('throttleit')
+
+export default class TTLMap extends Map {
+    constructor(iterable = [], ttl = 60000, evictInterval = 10000) {
+        super()
+        this.ttl = ttl
+        this.evictInterval = evictInterval
+        this.deleteExpired = throttle(
+            () => {
+                console.log('deleteExpired', Date.now())
+                this.ledger.forEach(
+                    (value, key) => {
+                        if(Date.now() > value) {
+                            this.ledger.delete(key)
+                            this.delete(key)
+                        }
+                    }
+                )
+                if(this.ledger.size)
+                    this.deleteExpired()
+            },
+            this.evictInterval
+        )
+        this.ledger = new Map()
+        for(const [key, value] of iterable)
+            this.set(key, value)
+    }
+    set(key, value) {
+        this.ledger.set(key, Date.now() + this.ttl)
+        this.deleteExpired()
+        return super.set(key, value)
+    }
+    get(key) {
+        const value = super.get(key)
+        if(value) {
+            this.ledger.set(key, Date.now() + this.ttl)
+            this.deleteExpired()
+        }
+        return value
+    }
+}
