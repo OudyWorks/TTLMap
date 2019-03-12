d51588c84da67b8e22bc78cefb6677687cf69f42
Ayoub Oudmane <ayoub@oudmane.me>
2019-03-12 22:59:47 +0100

    #

---
+++ index.js
@@ -0,0 +1,44 @@
+const throttle = require('lodash.throttle')
+
+module.exports = class TTLMap extends Map {
+  constructor(iterable = [], ttl = 60000, evictInterval = 10000, remove = function(value, key) {}) {
+    super()
+    this.ttl = ttl
+    this.evictInterval = evictInterval
+    this.deleteExpired = throttle(
+      () => {
+        this.ledger.forEach(
+          (value, key) => {
+            if (Date.now() > value) {
+              this.ledger.delete(key)
+              remove(super.get(key), key)
+              this.delete(key)
+            }
+          }
+        )
+        if (this.ledger.size)
+          this.deleteExpired()
+      },
+      this.evictInterval,
+      {
+        leading: false
+      }
+    )
+    this.ledger = new Map()
+    for (const [key, value] of iterable)
+      this.set(key, value)
+  }
+  set(key, value) {
+    this.ledger.set(key, Date.now() + this.ttl)
+    this.deleteExpired()
+    return super.set(key, value)
+  }
+  get(key) {
+    const value = super.get(key)
+    if (value) {
+      this.ledger.set(key, Date.now() + this.ttl)
+      this.deleteExpired()
+    }
+    return value
+  }
+}
